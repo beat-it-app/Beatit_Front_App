37d7320ef387c5b6ba4418c111fafbf001c7c878
송하은 <155309085+EunHaSong@users.noreply.github.com>
2026-06-16 04:02:09 +0900

    docs/#1: 디자인 테마 및 폰트 설정

---
+++ lib/src/core/theme/app_fonts.dart
@@ -0,0 +1,68 @@
+// lib/src/core/design_system/theme/app_text_theme.dart
+import 'package:flutter/material.dart';
+
+abstract class FontStyles {
+  static const _fontFamily = 'Pretendard';
+
+  static const TextStyle semi28 = TextStyle(
+    fontSize: 28,
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w600,
+    letterSpacing: -0.32,
+  );
+  static const TextStyle semi20 = TextStyle(
+    fontSize: 20,
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w600,
+    letterSpacing: -0.32,
+  );
+  static const TextStyle semi24 = TextStyle(
+    fontSize: 24,
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w600,
+    letterSpacing: -0.32,
+  );
+  static const TextStyle semi16 = TextStyle(
+    fontSize: 16,
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w600,
+    letterSpacing: -0.32,
+  );
+
+  static const TextStyle med20 = TextStyle(
+    fontSize: 20,
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w500,
+    letterSpacing: -0.32,
+  );
+  static const TextStyle med16 = TextStyle(
+    fontSize: 16,
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w500,
+    letterSpacing: -0.32,
+  );
+  static const TextStyle med15 = TextStyle(
+    fontSize: 15,
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w500,
+    letterSpacing: -0.32,
+  );
+  static const TextStyle med14 = TextStyle(
+    fontSize: 14,
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w500,
+    letterSpacing: -0.32,
+  );
+  static const TextStyle med12 = TextStyle(
+    fontSize: 12,
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w500,
+    letterSpacing: -0.32,
+  );
+  static const TextStyle med11 = TextStyle(
+    fontSize: 11,
+    fontFamily: _fontFamily,
+    fontWeight: FontWeight.w500,
+    letterSpacing: -0.32,
+  );
+}
