d0901e3a4e6a650152eea794e814796d72f034a9
송하은 <155309085+EunHaSong@users.noreply.github.com>
2026-07-01 04:02:00 +0900

    ui/#3: 아이디/비밀번호 찾기 결과 위젯 생성

---
+++ lib/src/domain/auth/widget/result_box.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'package:beatit_front_app/src/core/theme/app_theme_extension.dart';
+import 'package:beatit_front_app/src/core/theme/app_spacing.dart';
+import 'package:beatit_front_app/src/core/theme/app_fonts.dart';
+import 'package:beatit_front_app/src/core/theme/app_radius.dart';
+
+class ResultBox extends StatelessWidget {
+  final String label;
+  final String value;
+
+  const ResultBox({super.key, required this.label, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Container(
+      width: double.infinity,
+      height: 45,
+      decoration: BoxDecoration(
+        color: context.grays.gray8,
+        borderRadius: BorderRadius.circular(AppRadius.sm),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            '${label}: ',
+            style: FontStyles.semi16.copyWith(color: context.grays.gray1),
+          ),
+          const SizedBox(height: AppSpacing.x8),
+          Text(
+            value,
+            style: FontStyles.semi16.copyWith(color: context.grays.gray1),
+          ),
+        ],
+      ),
+    );
+  }
+}
