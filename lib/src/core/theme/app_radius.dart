6cd965304d67f209eb588859dae9c948f8209ed9
송하은 <155309085+EunHaSong@users.noreply.github.com>
2026-07-05 00:57:55 +0900

    style/#6: ButtonRedius xl > xxl로 수정 및 xl = 10으로 추가

--- lib/src/core/theme/app_radius.dart
+++ lib/src/core/theme/app_radius.dart
@@ -5,6 +5,7 @@ abstract final class AppRadius {
   static const double sm = 5;
   static const double md = 5.72;
   static const double lg = 6;
-  static const double xl = 20;
+  static const double xl = 10;
+  static const double xxl = 20;
   static const double pill = 999;
 }
