489318e72c47fbce04b439936425c0e4ac02843a
송하은 <155309085+EunHaSong@users.noreply.github.com>
2026-07-30 01:48:12 +0900

    feat/#10: appbar에 드롭다운 리스트 적용

--- lib/src/core/widgets/appbars/app_two_appbar.dart
+++ lib/src/core/widgets/appbars/app_two_appbar.dart
@@ -1,10 +1,10 @@
 import 'package:beatit_front_app/src/core/extensions/app_theme_extension.dart';
+import 'package:beatit_front_app/src/core/theme/app_fonts.dart';
 import 'package:beatit_front_app/src/core/theme/app_radius.dart';
 import 'package:beatit_front_app/src/core/theme/app_spacing.dart';
+import 'package:beatit_front_app/src/core/widgets/dropdowns/app_dropdown_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
-import '../../theme/app_fonts.dart';
 
 enum AppTwoAppBarTrailing { add, search, all }
 
@@ -16,6 +16,11 @@ class AppTwoAppBar extends StatelessWidget implements PreferredSizeWidget {
     this.trailing = AppTwoAppBarTrailing.add,
     this.onAddPressed,
     this.onSearchPressed,
+    this.addMenuItems = const [],
+    this.addMenuAlignment = AppDropdownAlignment.right,
+    this.addMenuWidth = 170,
+    this.addMenuItemHeight = 44,
+    this.addMenuOffset = const Offset(0, 8),
     this.toolbarHeight = 84,
   });
 
@@ -23,6 +28,11 @@ class AppTwoAppBar extends StatelessWidget implements PreferredSizeWidget {
     super.key,
     required String title,
     VoidCallback? onAddPressed,
+    this.addMenuItems = const [],
+    this.addMenuAlignment = AppDropdownAlignment.right,
+    this.addMenuWidth = 170,
+    this.addMenuItemHeight = 44,
+    this.addMenuOffset = const Offset(0, 8),
     double toolbarHeight = 84,
   }) : title = title,
        showBackButton = true,
@@ -40,6 +50,11 @@ class AppTwoAppBar extends StatelessWidget implements PreferredSizeWidget {
        trailing = AppTwoAppBarTrailing.search,
        onAddPressed = null,
        onSearchPressed = onSearchPressed,
+       addMenuItems = const [],
+       addMenuAlignment = AppDropdownAlignment.right,
+       addMenuWidth = 170,
+       addMenuItemHeight = 44,
+       addMenuOffset = const Offset(0, 8),
        toolbarHeight = toolbarHeight;
 
   const AppTwoAppBar.all({
@@ -47,6 +62,11 @@ class AppTwoAppBar extends StatelessWidget implements PreferredSizeWidget {
     String? title,
     VoidCallback? onAddPressed,
     VoidCallback? onSearchPressed,
+    this.addMenuItems = const [],
+    this.addMenuAlignment = AppDropdownAlignment.right,
+    this.addMenuWidth = 170,
+    this.addMenuItemHeight = 44,
+    this.addMenuOffset = const Offset(0, 8),
     double toolbarHeight = 84,
   }) : title = title,
        showBackButton = true,
@@ -59,8 +79,20 @@ class AppTwoAppBar extends StatelessWidget implements PreferredSizeWidget {
   final bool showBackButton;
   final AppTwoAppBarTrailing trailing;
 
+  /// 메뉴 항목이 없을 때 + 버튼이 직접 실행할 콜백
   final VoidCallback? onAddPressed;
+
   final VoidCallback? onSearchPressed;
+
+  /// + 버튼을 눌렀을 때 표시할 메뉴
+  ///
+  /// 빈 리스트이면 드롭다운을 표시하지 않고 onAddPressed를 실행한다.
+  final List<AppDropdownItem> addMenuItems;
+
+  final AppDropdownAlignment addMenuAlignment;
+  final double addMenuWidth;
+  final double addMenuItemHeight;
+  final Offset addMenuOffset;
 
   final double toolbarHeight;
 
@@ -88,39 +120,62 @@ class AppTwoAppBar extends StatelessWidget implements PreferredSizeWidget {
       actions: [
         Padding(
           padding: const EdgeInsets.only(right: AppSpacing.x16),
-          child: Row(
-            children: [
-              switch (trailing) {
-                AppTwoAppBarTrailing.add => _AppBarPressIconButton(
-                  icon: 'assets/icons/appbar/plus.svg',
-                  semanticLabel: '추가',
-                  onPressed: onAddPressed ?? _noop,
-                ),
-                AppTwoAppBarTrailing.search => _AppBarPressIconButton(
-                  icon: 'assets/icons/appbar/search.svg',
-                  semanticLabel: '검색',
-                  onPressed: onSearchPressed ?? _noop,
-                ),
-                AppTwoAppBarTrailing.all => Row(
-                  children: [
-                    _AppBarPressIconButton(
-                      icon: 'assets/icons/appbar/search.svg',
-                      semanticLabel: '검색',
-                      onPressed: onSearchPressed ?? _noop,
-                    ),
-                    const SizedBox(width: AppSpacing.x8),
-                    _AppBarPressIconButton(
-                      icon: 'assets/icons/appbar/plus.svg',
-                      semanticLabel: '추가',
-                      onPressed: onAddPressed ?? _noop,
-                    ),
-                  ],
-                ),
-              },
-            ],
-          ),
+          child: switch (trailing) {
+            AppTwoAppBarTrailing.add => _buildAddAction(),
+            AppTwoAppBarTrailing.search => _buildSearchAction(),
+            AppTwoAppBarTrailing.all => Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                _buildSearchAction(),
+                const SizedBox(width: AppSpacing.x8),
+                _buildAddAction(),
+              ],
+            ),
+          },
         ),
       ],
+    );
+  }
+
+  Widget _buildSearchAction() {
+    return _AppBarPressIconButton(
+      icon: 'assets/icons/appbar/search.svg',
+      semanticLabel: '검색',
+      onPressed: onSearchPressed ?? _noop,
+    );
+  }
+
+  Widget _buildAddAction() {
+    // 메뉴 항목이 없는 화면에서는 기존처럼 + 버튼 콜백을 직접 실행한다.
+    if (addMenuItems.isEmpty) {
+      return _AppBarPressIconButton(
+        icon: 'assets/icons/appbar/plus.svg',
+        semanticLabel: '추가',
+        onPressed: onAddPressed ?? _noop,
+      );
+    }
+
+    // 메뉴 항목이 존재하는 화면에서는 + 버튼이 드롭다운을 연다.
+    return AppDropdownList(
+      width: addMenuWidth,
+      itemHeight: addMenuItemHeight,
+      alignment: addMenuAlignment,
+      alignmentOffset: addMenuOffset,
+      anchorWidth: 60,
+      items: addMenuItems,
+      triggerBuilder: (context, controller) {
+        return _AppBarPressIconButton(
+          icon: 'assets/icons/appbar/plus.svg',
+          semanticLabel: '추가 메뉴',
+          onPressed: () {
+            if (controller.isOpen) {
+              controller.close();
+            } else {
+              controller.open();
+            }
+          },
+        );
+      },
     );
   }
 }
@@ -166,8 +221,6 @@ class _AppBarPressIconButtonState extends State<_AppBarPressIconButton> {
 
   @override
   Widget build(BuildContext context) {
-    final colors = Theme.of(context).colorScheme;
-
     return Semantics(
       button: true,
       label: widget.semanticLabel,
@@ -197,7 +250,15 @@ class _AppBarPressIconButtonState extends State<_AppBarPressIconButton> {
                   ? widget.pressInDuration
                   : widget.pressOutDuration,
               curve: widget.curve,
-              child: SvgPicture.asset(widget.icon, color: context.grays.gray5),
+              child: SvgPicture.asset(
+                widget.icon,
+                width: widget.iconSize,
+                height: widget.iconSize,
+                colorFilter: ColorFilter.mode(
+                  context.grays.gray5,
+                  BlendMode.srcIn,
+                ),
+              ),
             ),
           ),
         ),
