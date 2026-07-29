aea5c30b50773a417ef3a92932aa3fdb03bdc0bf
송하은 <155309085+EunHaSong@users.noreply.github.com>
2026-07-30 01:48:34 +0900

    test/#10: 드롭다운 리스트 테스트를 위한 페이지 추가

--- lib/dropdown_test_page.dart
+++ lib/dropdown_test_page.dart
@@ -5,6 +5,7 @@ import 'package:beatit_front_app/src/core/theme/app_spacing.dart';
 import 'package:beatit_front_app/src/core/widgets/appbars/app_top_appbar.dart';
 import 'package:beatit_front_app/src/core/widgets/appbars/app_two_appbar.dart';
 import 'package:beatit_front_app/src/core/widgets/buttons/app_button.dart';
+import 'package:beatit_front_app/src/core/widgets/dropdowns/app_dropdown_list.dart';
 import 'package:beatit_front_app/src/core/widgets/popups/app_popup.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/material.dart';
@@ -19,74 +20,58 @@ class DropdownTestPage extends StatefulWidget {
 class _DropdownTestPageState extends State<DropdownTestPage> {
   @override
   Widget build(BuildContext context) {
-    final colorScheme = Theme.of(context).colorScheme;
-    final textTheme = Theme.of(context).textTheme;
-
     return Scaffold(
-      appBar: AppTwoAppBar(trailing: AppTwoAppBarTrailing.all),
+      appBar: AppTwoAppBar(
+        trailing: AppTwoAppBarTrailing.all,
+        addMenuAlignment: AppDropdownAlignment.right,
+        addMenuOffset: const Offset(0, 68),
+
+        addMenuItems: [
+          AppDropdownItem(
+            label: '공지 작성하기',
+            onPressed: () {
+              debugPrint('공지 작성 화면 이동');
+            },
+          ),
+          AppDropdownItem(
+            label: '투표 생성하기',
+            onPressed: () {
+              debugPrint('투표 생성 화면 이동');
+            },
+          ),
+          AppDropdownItem(
+            label: '밋잇 생성하기',
+            onPressed: () {
+              debugPrint('밋잇 생성 화면 이동');
+            },
+          ),
+        ],
+
+        onSearchPressed: () {
+          debugPrint('공지 검색');
+        },
+      ),
+      // appBar: AppTopAppBar.backMore(
+      //   title: '이기주, 노영서, 이현영',
+      //   moreMenuOffset: const Offset(-16, 56),
+      //   moreMenuItems: [
+      //     AppDropdownItem(
+      //       label: '수정하기',
+      //       onPressed: () {
+      //         debugPrint('수정');
+      //       },
+      //     ),
+      //     AppDropdownItem(
+      //       label: '삭제하기',
+      //       onPressed: () {
+      //         debugPrint('삭제');
+      //       },
+      //     ),
+      //   ],
+      // ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
-        child: Column(
-          children: [
-            AppButton(text: '드롭다운 버튼', onPressed: () {}),
-            const SizedBox(height: 16),
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.transparent),
-                borderRadius: BorderRadius.circular(AppRadius.md),
-                color: context.grays.gray7.withOpacity(0.95),
-              ),
-              child: Column(
-                children: [
-                  InkWell(
-                    onTap: () async {},
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 16,
-                        vertical: 12,
-                      ),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(AppRadius.md),
-                        border: Border(
-                          bottom: BorderSide(color: context.grays.gray7),
-                        ),
-                      ),
-                      child: Row(
-                        children: [
-                          SvgPicture.asset(
-                            'assets/icons/check/check.svg',
-                            width: 24.0,
-                          ),
-                          const SizedBox(width: AppSpacing.x4),
-                          Text('팀 생성하기', style: FontStyles.med16),
-                        ],
-                      ),
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () async {},
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 16,
-                        vertical: 12,
-                      ),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(AppRadius.md),
-                        border: Border(
-                          bottom: BorderSide(color: context.grays.gray7),
-                        ),
-                        color: context.grays.white,
-                      ),
-                      child: Row(
-                        children: [Text('팀 생성하기', style: FontStyles.med16)],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+        child: Column(children: []),
       ),
     );
   }
