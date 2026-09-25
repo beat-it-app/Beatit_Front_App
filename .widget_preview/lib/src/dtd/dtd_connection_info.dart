9ec3a8dbc71d551ec53ea9da3baf926a09f1f2f1
송하은 <155309085+EunHaSong@users.noreply.github.com>
2026-09-25 16:49:53 +0900

    feat/#35: 팀 페이지 선택, 가입 연동

---
+++ .widget_preview/lib/src/dtd/dtd_connection_info.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: implementation_imports
+
+const String kWidgetPreviewDtdUri = 'ws://127.0.0.1:55694/6WVlLxSkCNI=';
+const String kWidgetPreviewService =
+    'widget-preview-7d9160fb-c88b-4a3c-86ad-9ddde636afac';
+const String kWidgetPreviewScaffoldStream =
+    'WidgetPreviewScaffold-7d9160fb-c88b-4a3c-86ad-9ddde636afac';
+const String kProjectRootPath = r'C:\SHE_WORK\BeatIt\Beatit_Front_App';
